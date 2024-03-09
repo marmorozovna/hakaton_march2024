2a820f54cac92b0221647e743d7e88d261098417
AllaMB <126911591+AllaMB@users.noreply.github.com>
2024-03-09 14:34:14 +0100

    add JS logic to put tasks under the appropriate category

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.btnAddTask').addEventListener('click', addTask);
 });
 
+function addTask() {
+    // извлекаю task input и selected category
+    var taskInput = document.getElementById('task-input').value;
+    var selectedCategory = document.getElementById('selectedImportance').value;
+
+    // проверяем если task input пустой
+    if (taskInput.trim() === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    // создаем task element
+    var taskElement = document.createElement('li');
+    taskElement.textContent = taskInput;
+
+    // добавляем задачу в нужный квадрант
+    var quadrantTasks = document.getElementById(selectedCategory + 'Tasks');
+    quadrantTasks.appendChild(taskElement);
+
+    // очищаем поле ввода задачи (task input field)
+    document.getElementById('task-input').value = '';
+}
